52e9c2623869fc9ca0f96e5d90dd5136707c13e5
Dave Hylands <dhylands@gmail.com>
2017-09-14 14:27:17 -0700

    A couple scripts for loop mount/unmounting .img files.

---
+++ tools/mount-img.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+#
+# Script which loop mounts an image
+
+SCRIPT_NAME=$(basename $0)
+
+VERBOSE=0
+ROOT_MOUNTED=0
+LEAVE_MOUNTED=0
+
+###########################################################################
+#
+# Prints the program usage
+#
+usage() {
+  echo "Usage: ${SCRIPT_NAME} [-v] IMAGE_FILE"
+}
+
+###########################################################################
+#
+# Parses command line arguments and run the program.
+#
+main () {
+  while getopts "v" opt "$@"; do
+    case $opt in
+      v)
+        VERBOSE=1
+        ;;
+      ?)
+        echo "Unrecognized option: ${opt}"
+        usage
+        exit 1
+        ;;
+      esac
+  done
+  shift $((${OPTIND} - 1))
+  IMAGE_FILE=$1
+  if [ -z "${IMAGE_FILE}" ]; then
+    echo "No image file provided."
+    usage
+    exit 1
+  fi
+
+  if [ "${VERBOSE}" == "1" ]; then
+    echo "Image File: ${IMAGE_FILE}"
+  fi
+
+  if [ ! -f "${IMAGE_FILE}" ]; then
+    echo "Image File: '${IMAGE_FILE}' not a file."
+    exit 1
+  fi
+  
+  # Figure out the device names that kpartx will create
+  declare -a KPARTX_DEVS
+  KPARTX_DEVS=($(while read dev rest; do
+    echo "/dev/mapper/${dev}"
+  done < <(sudo kpartx -v -l "${IMAGE_FILE}")))
+
+  BOOT_DEV=${KPARTX_DEVS[0]}
+  ROOT_DEV=${KPARTX_DEVS[1]}
+
+  ROOT_MOUNTPOINT=rpi-root
+  MOZILLA_IOT_DIR=${ROOT_MOUNTPOINT}/home/pi/mozilla-iot
+
+  if [ "${VERBOSE}" == "1" ]; then
+    echo "  BOOT_DEV: ${BOOT_DEV}"
+    echo "  ROOT_DEV: ${ROOT_DEV}"
+  fi
+
+  # Create the loop mounts
+  if sudo kpartx -v -a "${IMAGE_FILE}"; then
+    # It seems that there is sometimes a race and that the loop files don't
+    # always exist by the time kpartx exits, so we wait until we actually
+    # see the loop file before continuing.
+    iter=0
+    while [ ! -e ${ROOT_DEV} ]; do
+      echo "Waiting for loop mount to be created"
+      sleep 1
+      iter=$(( ${iter} + 1 ))
+      if [ ${iter} -gt 5 ]; then
+        echo "Timeout waiting for loop mount"
+        exit 1
+      fi
+    done
+    echo "Loop mounts created successfully"
+  else
+    echo "Failed to create loop mounts"
+    exit 1
+  fi
+  LOOP_MOUNT_CREATED=1
+
+  # Mount the root parition of the image
+  mkdir -p ${ROOT_MOUNTPOINT}
+  if sudo mount ${ROOT_DEV} ${ROOT_MOUNTPOINT}; then
+    echo "Root partition mounted successfully"
+  else
+    echo "Failed to mount root partition"
+    exit 1
+  fi
+  ROOT_MOUNTED=1
+
+  LEAVE_MOUNTED=1
+}
+
+###########################################################################
+#
+# Do required cleanup. We use a trap so that if the script dies for any
+# reason then we'll still undo whatever is needed.
+#
+function cleanup() {
+  if [ "${LEAVE_MOUNTED}" == 1 ]; then
+    return
+  fi
+  if [ "${ROOT_MOUNTED}" == 1 ]; then
+    echo "Unmounting ${ROOT_DEV}"
+    sudo umount ${ROOT_MOUNTPOINT}
+    ROOT_MOUNTED=0
+  fi
+  if [ -d ${ROOT_MOUNTPOINT} ]; then
+    sudo rmdir ${ROOT_MOUNTPOINT}
+  fi
+  if [ "${LOOP_MOUNT_CREATED}" == 1 ]; then
+    echo "Removing loop mounts"
+    sudo kpartx -v -d "${IMAGE_FILE}"
+    LOOP_MOUNT_CREATED=0
+  fi
+}
+
+trap cleanup EXIT
+
+main "$@"
---
+++ tools/umount-img.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Script which loop mounts an image
+
+SCRIPT_NAME=$(basename $0)
+
+VERBOSE=0
+ROOT_MOUNTED=0
+LEAVE_MOUNTED=0
+
+###########################################################################
+#
+# Prints the program usage
+#
+usage() {
+  echo "Usage: ${SCRIPT_NAME} [-v] IMAGE_FILE"
+}
+
+###########################################################################
+#
+# Parses command line arguments and run the program.
+#
+main () {
+  while getopts "v" opt "$@"; do
+    case $opt in
+      v)
+        VERBOSE=1
+        ;;
+      ?)
+        echo "Unrecognized option: ${opt}"
+        usage
+        exit 1
+        ;;
+      esac
+  done
+  shift $((${OPTIND} - 1))
+  IMAGE_FILE=$1
+  if [ -z "${IMAGE_FILE}" ]; then
+    echo "No image file provided."
+    usage
+    exit 1
+  fi
+
+  if [ "${VERBOSE}" == "1" ]; then
+    echo "Image File: ${IMAGE_FILE}"
+  fi
+
+  if [ ! -f "${IMAGE_FILE}" ]; then
+    echo "Image File: '${IMAGE_FILE}' not a file."
+    exit 1
+  fi
+  
+  ROOT_MOUNTPOINT=rpi-root
+
+  # Unmount the root parition of the image
+  if sudo umount ${ROOT_MOUNTPOINT}; then
+    echo "Root partition unmounted successfully"
+  else
+    echo "Failed to unmount root partition"
+    exit 1
+  fi
+
+  # Remove the loop mounts
+  if sudo kpartx -v -d "${IMAGE_FILE}"; then
+    echo "Loop mounts removed successfully"
+  else
+    echo "Failed to remove loop mounts"
+    exit 1
+  fi
+
+  rmdir ${ROOT_MOUNTPOINT}
+}
+
+main "$@"
