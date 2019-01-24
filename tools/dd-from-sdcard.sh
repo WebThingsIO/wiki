3fdf5c51df7f966c8dc913103a2397371229da51
Dave Hylands <dhylands@gmail.com>
2019-01-23 16:28:30 -0800

    Added more checks on the device

--- tools/dd-from-sdcard.sh
+++ tools/dd-from-sdcard.sh
@@ -12,6 +12,24 @@ if [ -z "${DD_DEV}" ]; then
   usage
   exit 1
 fi
+DD_NAME=$(basename ${DD_DEV})
+if [[ "${DD_DEV:0:1}" != '/' ]]; then
+  DD_DEV="/dev/${DD_DEV}"
+fi
+REMOVABLE=$(cat /sys/block/${DD_NAME}/removable)
+if [ "${REMOVABLE}" != "1" ]; then
+  echo "${DD_DEV} isn't a removable drive"
+  exit 1
+fi
+MEDIA_SIZE=$(cat /sys/block/${DD_NAME}/size)
+if [ "${MEDIA_SIZE}" == "0" ]; then
+  echo "No media present at ${DD_DEV}"
+  exit 1
+fi
+if [ $(( ${MEDIA_SIZE} / 2048 / 1024 )) -gt 100 ]; then
+  echo "${DD_DEV} media size is larger than 100 Gb - wrong device?"
+  exit 1
+fi
 
 IMG_FILENAME="$2"
 if [ -z "${IMG_FILENAME}" ]; then
